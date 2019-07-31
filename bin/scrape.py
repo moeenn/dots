4ed4bb78f81d2ca72cc18015b66611d59f26c70e
moeenn <moeen.v8@gmail.com>
2019-07-31 21:21:22 +0500

    31/July/2019 - 09:21:22 PM

--- bin/scrape.py
+++ bin/scrape.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3.6
+
 """
 This script has following dependencies
 1. BeautifulSoup4
@@ -12,7 +14,8 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import wget
-from os import mkdir,chdir
+from os import mkdir
+from sys import exit
 
 # determine number of pages of results
 # return: Int
@@ -23,20 +26,22 @@ def get_totalPages(url):
 
     # get table row containing page numbers
     pages_row = soup.find('td', class_='pages')
-
     pages_row_cells = pages_row.find_all('a')
 
+	# contents of the cells containing page numbers
     pages_row_cells_data = []
 
     for link in pages_row_cells:
         pages_row_cells_data.append(link.text)
 
+	# count total number of pages
     total_pages = 0
-
     for data in pages_row_cells_data:
         try:
+			# if cell data converts to number it means that it is a page number
             data = int(data)
         except:
+			# if conversion of cell data to number fails, it means there are no more page numbers
             break
         else:
             total_pages += 1
@@ -66,13 +71,11 @@ def page_galleries(url):
     # get the main section of the page
     gallery_grid = soup.find('div', id='content')
 
-    # get the list of galleries on page
+    # get the list of galleries on page (i.e. gallery tokens)
     page_galleries = gallery_grid.find_all('div', class_='pic')
 
-    # get the gallery tokens
-    page_galleries_urls = []
-
     # construct the complete urls for galleries
+    page_galleries_urls = []
     for gallery in page_galleries:
         gallery_token = gallery.a["href"]
         complete_url = f'https://fuskator.com{ gallery_token }'
@@ -99,9 +102,9 @@ def get_allGalleries(model_name):
     # generate list of result page urls
     pageURLS = get_pageURLS(model_name, total_pages)
 
+    # gather gallery links from all pages of results
     all_galleries = []
 
-    # gather gallery links from all pages of results
     for url in pageURLS:
         print(f'Scoping {url}')
         all_galleries += page_galleries(url)
@@ -113,39 +116,40 @@ def get_allGalleries(model_name):
 # get list of image urls from single gallery page
 # return: list
 def get_galleryImageURLS(url):
-    print('Resolving JavaScript and ', end='')
-    # create a new Firefox session
-    # IMPORTANT: make sure the geckodriver is in our path
-    try:
-        driver = webdriver.Firefox()
-    except:
-        message = 'You forgot to install the geckodriver in your $PATH.\n ' 
-        message += 'Get it Here: https://github.com/mozilla/geckodriver/releases'
-        raise Exception(message)
-    else:
-        driver.implicitly_wait(40)
-        driver.get(url)
+	print('Resolving JavaScript and ', end='')
+	# create a new Browser session
+	try:
+		options = webdriver.ChromeOptions()
+		# headless means the browser will be invisible
+		options.add_argument("headless")
+		driver = webdriver.Chrome(chrome_options=options)
+	except:
+		message = 'Chromedriver is not available in your $PATH.' 
+		raise Exception(message)
+	else:
+		driver.implicitly_wait(40)
+		driver.get(url)
 
-        # make the url workable with BeautifulSoup
-        print('Scoping the gallery')
-        soup = BeautifulSoup(driver.page_source, 'lxml')
+		# bring the completed page into BeautifulSoup
+		print('Scoping the gallery')
+		soup = BeautifulSoup(driver.page_source, 'lxml')
 
-        # close the driver window
-        driver.close()
+		# close the driver window
+		driver.close()
 
-        # get the relevant section of the page
-        thumbs_grid = soup.find('div', id='thumbimages')
+		# get the relevant section of the page
+		thumbs_grid = soup.find('div', id='thumbimages')
 
-        #get urls of gallery images
-        gallery_images = thumbs_grid.find_all('div', class_='pic')
+		#get urls of gallery images
+		gallery_images = thumbs_grid.find_all('div', class_='pic')
 
-        image_urls = []
+		image_urls = []
 
-        for image in gallery_images:
-            url = f'https:{image.a["href"]}'
-            image_urls.append(url)
+		for image in gallery_images:
+			url = f'https:{image.a["href"]}'
+			image_urls.append(url)
 
-        return image_urls
+		return image_urls
 
 
 
@@ -172,30 +176,65 @@ def read_and_download(file, destination):
     try:
         mkdir(destination)
     except:
-        raise Exception("Target directory already exists")
-    else:
+        pass
+    finally:
         with open(file, 'rt') as url_dump_file:
             # read url one by one
             for url in url_dump_file:
                 #save the file with this name
                 filename = url.split('/')[-1]
+                retries = 0
 
-                #download and create the file
-                #print(f'\nDownloading {filename}')
-                wget.download(url.strip(), f'{destination}/{filename}')
+                while True:
+                    try:
+                        #download and create the file
+                        #print(f'\nDownloading {filename}')
+                        wget.download(url.strip(), f'{destination}/{filename}')
+                    except:
+                        retries += 1
+                        if retries == 3:
+                            break
 
 
 if __name__ == '__main__':
-    model_name = input('Enter Name: ').lower()
+	model_name = input('Enter Name: ').lower()
+	# format the name of the model 
+	model_name = model_name.replace(' ', '_')
 
-    # format the name of the model 
-    model_name = model_name.replace(' ', '_')
+	# options menu
+	print('Select Option:')
+	print('1. Generate Images URL Dump')
+	print('2. Download Images from Dump')
+	print('3. Both Option 1 & 2')
+	print('4. Exit')
 
-    # get urls of images from all galleries and dump them into a file
-    dump_file = f'{model_name}.txt'
+	# get the selection from the user
+	while True:
+		try:
+			selection = int(input('Selection: '))
+			break
+		except:
+			print('Error: Invalid Input')
 
-    # create the dump of image urls
-    create_dump(model_name, dump_file)
+	# name of the dump file
+	dump_file = f'{model_name}.txt'
 
-    # read urls from dump file and download them
-    read_and_download(dump_file, model_name)
+	try:
+		# case the selection
+		if selection == 1:
+			# create the dump of image urls
+			create_dump(model_name, dump_file)
+		elif selection == 2:
+			# read urls from dump file and download them
+			read_and_download(dump_file, model_name)
+		elif selection == 3:
+			create_dump(model_name, dump_file)
+			read_and_download(dump_file, model_name)
+		elif selection == 4:
+			print('Exiting ...')
+			exit(0)
+		else:
+			raise Exception('Invalid Input')
+			exit(1)
+	except Exception as err:
+		print(f'Error: {err}')
