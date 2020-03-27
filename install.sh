41e60f7ffaf209c5f6f9ac5ab8925286b9aaece2
moeenn <moeen.v8@gmail.com>
2020-03-27 19:55:01 +0500

    March 27, 2020 - 07:55:01 PM
    polish install script

--- install.sh
+++ install.sh
@@ -13,17 +13,17 @@ echo "\033[33mRemoving Dots Archive\033[0m";
 rm -vf master.zip;
 
 # place the core dots folder
-ln -s -r ./dots-master/ $HOME/.dots/;
+ln -srf ./dots-master/ $HOME/.dots/;
 
 # link the conf_dir content
-ln -s -r $HOME/.dots/configs/config $HOME/.config;
+ln -srf $HOME/.dots/configs/config $HOME/.config;
 
 # link home folder dots
-ln -s -r $HOME/.dots/configs/home/.* $HOME/;
+ln -srf $HOME/.dots/configs/home/.* $HOME/;
 
 # copy xorg configs
 echo "\033[32mXorg Tear-free Configs need to be installed System-wide. Please provide authentication\033[0m";
-sudo ln -s -r $HOME/.dots/configs/others/Xorg/xorg.conf.d /etc/X11/;
+sudo ln -srf $HOME/.dots/configs/others/Xorg/xorg.conf.d /etc/X11/;
 
 # diplay command for installation of common packages
 echo "\033[32mCommon applications and utilities can be installed using following command\033[0m";
