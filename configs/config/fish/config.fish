eb771885800d7cc90a39f2435579fdeecc3db5d2
moeenn <moeen.v8@gmail.com>
2023-01-27 12:25:25 +0500

    misc. updates

--- configs/config/fish/config.fish
+++ configs/config/fish/config.fish
@@ -100,4 +100,3 @@ alias bright "sudo brightnessctl -d 'intel_backlight' -set"
 # programming
 alias py "python3"
 alias dc "docker-compose"
-alias laraclear "php artisan config:clear; php artisan cache:clear; php artisan view:clear; php artisan route:clear; composer dump-autoload; composer dump"
