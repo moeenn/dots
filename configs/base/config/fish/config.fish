361192ebd49d16f052a6fcfe15082d25ecbeb8bb
moeenn <moeen.v8@gmail.com>
2023-10-10 18:27:40 +0500

    dots updated

--- configs/base/config/fish/config.fish
+++ configs/base/config/fish/config.fish
@@ -11,13 +11,10 @@ set fish_greeting
 # --------------------------------------------------------------------
 set SYSPATH /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin {$HOME}/.bin {$HOME}/.local/bin
 set FLATPAK_PATH /var/lib/flatpak/exports/bin
-set GOPATH {$HOME}/.go
 set NODEBIN {$HOME}/.npm/bin
-set GOBIN {$GOPATH}/bin
-set RUSTBIN {$HOME}/.cargo/bin
-set SCALABIN {$HOME}/.config/coursier/bin
+set RUBYBIN {$HOME}/.gem/ruby/3.0.0/bin
 
-set -U fish_user_paths {$SYSPATH} {$FLATPAK_PATH} {$NODEBIN} {$GOBIN} {$RUSTBIN} {$SCALABIN}
+set -U fish_user_paths {$SYSPATH} {$FLATPAK_PATH} {$NODEBIN} {$RUBYBIN}
 
 # --------------------------------------------------------------------
 #
--- configs/base/home/.profile
+++ configs/base/home/.profile
@@ -2,13 +2,9 @@ LC_CTYPE=en_US.UTF-8
 XDG_DATA_HOME=${HOME}/.config
 FLATPAKBIN=/var/lib/flatpak/exports/bin
 NODEBIN=${HOME}/.npm/bin
-GOPATH=${HOME}/.go
-GOBIN=${GOPATH}/bin
-RUSTBIN=${HOME}/.cargo/bin
-SCALABIN=${HOME}/.config/coursier/bin
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 PATH=${PATH}:${FLATPAKBIN}:${PATH}:${HOME}/.bin:${HOME}/.local/bin
-PATH=${PATH}:${NODEBIN}:${GOBIN}:${RUSTBIN}:${SCALABIN}
+PATH=${PATH}:${NODEBIN}
 
-export LC_CTYPE XDG_DATA_HOME PATH GOPATH 
+export LC_CTYPE XDG_DATA_HOME PATH
