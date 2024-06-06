8d2535fcc847e632bbaf7fd23b2b2b602c926320
moeenn <moeen.v8@gmail.com>
2024-06-06 03:18:42 +0300

    updated dots

--- setup/golang.sh
+++ setup/golang.sh
@@ -12,3 +12,17 @@ tar -xvf $FILENAME
 sudo mv go /usr/local
 
 cd $CURRENT_DIR
+
+# install necessary tools
+sudo apt-get install -y delve golang-honnef-go-tools-dev
+
+# lsp tools
+go install golang.org/x/tools/gopls@latest
+go install github.com/nametake/golangci-lint-langserver@latest
+
+# linting tools
+go install honnef.co/go/tools/cmd/staticcheck@latest
+go install github.com/kisielk/errcheck@latest
+
+# optional listers
+go install github.com/jgautheron/goconst/cmd/goconst@latest
--- setup/python.sh
+++ setup/python.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sudo apt-get install -y python3-pip &&
-pip3 install pyright ruff-lsp
+sudo apt-get install -y python3-pip python3-venv;
+pip3 install pyright ruff-lsp;
