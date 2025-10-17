dc3e50473d71b25ba57cd6b3573ce8d6dd3616ef
moeenn <moeen.v8@gmail.com>
2025-10-17 23:57:36 +0500

    cleaned up env files

--- configs/base/config/fish/config.fish
+++ configs/base/config/fish/config.fish
@@ -24,8 +24,7 @@ set USER_PATH {$HOME}/.bin {$HOME}/.local/bin
 set NODEBIN {$HOME}/.npm/bin
 set GOINSTALL /usr/local/go/bin
 set GOBIN {$HOME}/go/bin
-set OPAMBIN {$HOME}/.opam/default/bin
-set -U fish_user_paths {$SYSPATH} {$FLATPAK_PATH} {$USER_PATH} {$NODEBIN} {$GOINSTALL} {$GOBIN} {$OPAMBIN}
+set -U fish_user_paths {$SYSPATH} {$FLATPAK_PATH} {$USER_PATH} {$NODEBIN} {$GOINSTALL} {$GOBIN}
 
 # --------------------------------------------------------------------
 #
@@ -78,11 +77,10 @@ alias pm "python3 -m"
 alias delete_dstore "find . -name ".DS_Store" -type f -delete -print"
 alias m mvn
 
-
 # BEGIN opam configuration
 # This is useful if you're using opam as it adds:
 #   - the correct directories to the PATH
 #   - auto-completion for the opam binary
 # This section can be safely removed at any time if needed.
-test -r '/home/moeenn/.opam/opam-init/init.fish' && source '/home/moeenn/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+test -r '/home/moeenn/.opam/opam-init/init.fish' && source '/home/moeenn/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
 # END opam configuration
--- configs/base/home/.profile
+++ configs/base/home/.profile
@@ -6,19 +6,9 @@ FLATPAKBIN=/var/lib/flatpak/exports/bin
 NODEBIN=${HOME}/.npm/bin
 GOINSTALL=/usr/local/go/bin
 GOBIN=${HOME}/go/bin
-OPAMBIN=${HOME}/.opam/default/bin
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 PATH=${PATH}:${FLATPAKBIN}:${PATH}:${HOME}/.bin:${HOME}/.local/bin
-PATH=${PATH}:${GOINSTALL}:${NODEBIN}:${GOINSTALL}:${GOBIN}:${OPAMBIN}
+PATH=${PATH}:${NODEBIN}:${GOINSTALL}:${GOBIN}
 
 export DISPLAY LC_CTYPE XDG_DATA_HOME XDG_DATA_DIRS PATH
-
-
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-test -r '/home/moeenn/.opam/opam-init/init.sh' && . '/home/moeenn/.opam/opam-init/init.sh' > /dev/null 2> /dev/null || true
-# END opam configuration
