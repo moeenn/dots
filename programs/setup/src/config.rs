283ca89ea88580b9e697f2c3751d0f7343959806
moeenn <moeen.v8@gmail.com>
2025-11-29 00:57:10 +0500

    updated setup program

--- programs/setup/src/config.rs
+++ programs/setup/src/config.rs
@@ -6,6 +6,13 @@ use serde::Deserialize;
 use std::{fs, process::Command};
 use toml;
 
+fn get_username() -> Result<String, ExecutionError> {
+    match std::env::var("USER") {
+        Ok(user) => Ok(user),
+        Err(_) => Err(ExecutionError::UsernameReadFailed),
+    }
+}
+
 #[derive(Deserialize)]
 pub struct ConfigSystemPackages {
     base_packages: Vec<String>,
@@ -34,6 +41,19 @@ impl ConfigSystemPackages {
             false => Err(ExecutionError::BasePackageInstallationFailed),
         }
     }
+
+    fn install_common_apps(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("apt-get").arg("install").arg("-y");
+        for app in self.common_apps.iter() {
+            cmd.arg(app);
+        }
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::CommonAppsInstallationFailed),
+        }
+    }
 }
 
 impl Executable for ConfigSystemPackages {
@@ -43,14 +63,181 @@ impl Executable for ConfigSystemPackages {
 
         log::info("\ninstalling base packages");
         self.install_base_packages()?;
+
+        log::info("\ninstalling common apps");
+        self.install_common_apps()?;
         Ok(())
     }
 }
 
 #[derive(Deserialize)]
 pub struct ConfigFlatpak {
-    system_packages: Vec<String>,
     packages: Vec<String>,
+}
+
+impl ConfigFlatpak {
+    fn install_flatpak_support(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("apt-get").arg("install").arg("-y").arg("flatpak");
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::FlatpakSupportInstallationFailed),
+        }
+    }
+
+    fn configrure_flatpaks(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("flatpak")
+            .arg("remote-add")
+            .arg("--if-not-exists")
+            .arg("flathub")
+            .arg("https://dl.flathub.org/repo/flathub.flatpakrepo");
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::FlatpakConfigurationFailed),
+        }
+    }
+
+    fn install_flatpaks(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("flatpak").arg("install").arg("flathub").arg("-y");
+        for pkg in self.packages.iter() {
+            cmd.arg(pkg);
+        }
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::FlatpkaInstalltionFailed),
+        }
+    }
+}
+
+impl Executable for ConfigFlatpak {
+    fn execute(&self) -> Result<(), ExecutionError> {
+        log::info("\ninstalling flatpak support");
+        self.install_flatpak_support()?;
+
+        log::info("\nconfiguring flatpak repositories");
+        self.configrure_flatpaks()?;
+
+        log::info("\ninstalling flatpaks");
+        self.install_flatpaks()?;
+        Ok(())
+    }
+}
+
+#[derive(Deserialize)]
+pub struct ConfigDocker {
+    system_packages: Vec<String>,
+}
+
+impl ConfigDocker {
+    fn install_support(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("apt-get").arg("install").arg("-y");
+        for pkg in self.system_packages.iter() {
+            cmd.arg(pkg);
+        }
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::DockerInstallationFailed),
+        }
+    }
+
+    fn add_docker_group(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("groupadd").arg("docker");
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::DockerGroupAddFailed),
+        }
+    }
+
+    fn add_user_to_docker_group(&self, user: String) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("usermod").arg("-aG").arg("docker").arg(user);
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::DockerUserGroupAddFailed),
+        }
+    }
+}
+
+impl Executable for ConfigDocker {
+    fn execute(&self) -> Result<(), ExecutionError> {
+        log::info("\ninstalling docker support");
+        self.install_support()?;
+
+        log::info("\nadding docker group to the system");
+        self.add_docker_group()?;
+
+        let user = get_username()?;
+        log::info("\nadding current user to the docker group");
+        self.add_user_to_docker_group(user)?;
+
+        Ok(())
+    }
+}
+
+pub struct ConfigFish {}
+
+impl ConfigFish {
+    fn install_fish(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("apt-get").arg("install").arg("-y").arg("fish");
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::FishInstallFailed),
+        }
+    }
+
+    fn whereis_fish(&self) -> Result<String, ExecutionError> {
+        let mut cmd = Command::new("which");
+        cmd.arg("fish");
+
+        match cmd.output() {
+            Err(_) => Err(ExecutionError::FishLocationDetectFailed),
+            Ok(out) => match String::from_utf8(out.stdout) {
+                Ok(p) => Ok(p.trim().to_owned()),
+                Err(_) => Err(ExecutionError::FishLocationDetectFailed),
+            },
+        }
+    }
+
+    fn set_fish_as_default(
+        &self,
+        user: String,
+        fish_location: String,
+    ) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("usermod").arg("-s").arg(fish_location).arg(user);
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::FishSetDefaultFailed),
+        }
+    }
+}
+
+impl Executable for ConfigFish {
+    fn execute(&self) -> Result<(), ExecutionError> {
+        log::info("\ninstalling fish shell");
+        self.install_fish()?;
+
+        let username = get_username()?;
+        let fish_location = self.whereis_fish()?;
+
+        log::info("\nsetting fish as default shell");
+        self.set_fish_as_default(username, fish_location)?;
+
+        Ok(())
+    }
 }
 
 #[derive(Deserialize)]
@@ -72,19 +259,101 @@ pub struct ConfigPython {
     pip_packages: Vec<String>,
 }
 
+impl ConfigPython {
+    fn install_packages(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("apt-get").arg("install").arg("-y");
+        for pkg in self.system_packages.iter() {
+            cmd.arg(pkg);
+        }
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::PythonInstallationFailed),
+        }
+    }
+
+    fn install_pip_packages(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("pipx");
+        cmd.arg("install");
+        for pkg in self.pip_packages.iter() {
+            cmd.arg(pkg);
+        }
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::PythonPipPackagesInstallationFailed),
+        }
+    }
+}
+
+impl Executable for ConfigPython {
+    fn execute(&self) -> Result<(), ExecutionError> {
+        log::info("\ninstalling python");
+        self.install_packages()?;
+
+        log::info("\ninstalling pip packages");
+        self.install_pip_packages()?;
+
+        Ok(())
+    }
+}
+
 #[derive(Deserialize)]
 pub struct ConfigJava {
     system_packages: Vec<String>,
 }
 
-#[derive(Deserialize)]
-pub struct ConfigDocker {
-    system_packages: Vec<String>,
+impl ConfigJava {
+    fn install_packages(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("apt-get").arg("install").arg("-y");
+
+        for pkg in self.system_packages.iter() {
+            cmd.arg(pkg);
+        }
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::JavaInstallationFailed),
+        }
+    }
+}
+
+impl Executable for ConfigJava {
+    fn execute(&self) -> Result<(), ExecutionError> {
+        log::info("\ninstalling java");
+        self.install_packages()?;
+        Ok(())
+    }
 }
 
 #[derive(Deserialize)]
 pub struct ConfigXorg {
     system_packages: Vec<String>,
+}
+
+impl ConfigXorg {
+    fn install_packages(&self) -> Result<(), ExecutionError> {
+        let mut cmd = Command::new("sudo");
+        cmd.arg("apt-get").arg("install").arg("-y");
+        for pkg in self.system_packages.iter() {
+            cmd.arg(pkg);
+        }
+
+        match cmd.status().is_ok() {
+            true => Ok(()),
+            false => Err(ExecutionError::XorgPackagesInstallationFailed),
+        }
+    }
+}
+
+impl Executable for ConfigXorg {
+    fn execute(&self) -> Result<(), ExecutionError> {
+        log::info("\ninstalling xorg packages");
+        self.install_packages()?;
+        Ok(())
+    }
 }
 
 #[derive(Deserialize)]
@@ -102,6 +371,20 @@ pub struct Config {
 impl Config {
     pub fn execute(&self) -> Result<(), ExecutionError> {
         self.system_packages.execute()?;
+        self.flatpak.execute()?;
+        self.docker.execute()?;
+        self.xorg.execute()?;
+
+        let fish = ConfigFish {};
+        fish.execute()?;
+
+        self.python.execute()?;
+        self.java.execute()?;
+
+        // TODO:
+        // - implement cpp.
+        // - implment nodejs.
+
         Ok(())
     }
 }
--- programs/setup/src/config/executable.rs
+++ programs/setup/src/config/executable.rs
@@ -1,16 +1,76 @@
 pub enum ExecutionError {
+    UsernameReadFailed,
     MirrorsUpdateFailed,
     BasePackageInstallationFailed,
+    CommonAppsInstallationFailed,
+    FlatpakSupportInstallationFailed,
+    FlatpakConfigurationFailed,
+    FlatpkaInstalltionFailed,
+    DockerInstallationFailed,
+    DockerGroupAddFailed,
+    DockerUserGroupAddFailed,
+    FishInstallFailed,
+    FishLocationDetectFailed,
+    FishSetDefaultFailed,
+    PythonInstallationFailed,
+    PythonPipPackagesInstallationFailed,
+    JavaInstallationFailed,
+    XorgPackagesInstallationFailed,
 }
 
 impl std::fmt::Display for ExecutionError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            ExecutionError::UsernameReadFailed => {
+                write!(f, "failed to read $USER variable")
+            }
             ExecutionError::MirrorsUpdateFailed => {
                 write!(f, "failed to update packae manager mirrors")
             }
             ExecutionError::BasePackageInstallationFailed => {
                 write!(f, "failed to install base packages")
+            }
+            ExecutionError::CommonAppsInstallationFailed => {
+                write!(f, "failed to install common application")
+            }
+            ExecutionError::FlatpakSupportInstallationFailed => {
+                write!(f, "failed to install flatpak support")
+            }
+            ExecutionError::FlatpakConfigurationFailed => {
+                write!(f, "failed to configure flatpak repository")
+            }
+            ExecutionError::FlatpkaInstalltionFailed => {
+                write!(f, "failed to install flatpaks")
+            }
+            ExecutionError::DockerInstallationFailed => {
+                write!(f, "failed to install docker support")
+            }
+            ExecutionError::DockerGroupAddFailed => {
+                write!(f, "failed to add docker group to the system")
+            }
+            ExecutionError::DockerUserGroupAddFailed => {
+                write!(f, "failed to add current user to docker group")
+            }
+            ExecutionError::FishInstallFailed => {
+                write!(f, "failed to install fish shell")
+            }
+            ExecutionError::FishLocationDetectFailed => {
+                write!(f, "failed to determine fish shell installation location")
+            }
+            ExecutionError::FishSetDefaultFailed => {
+                write!(f, "failed to set fish shell as default")
+            }
+            ExecutionError::PythonInstallationFailed => {
+                write!(f, "failed to install python")
+            }
+            ExecutionError::PythonPipPackagesInstallationFailed => {
+                write!(f, "failed to install python pip packages")
+            }
+            ExecutionError::JavaInstallationFailed => {
+                write!(f, "failed to install java")
+            }
+            ExecutionError::XorgPackagesInstallationFailed => {
+                write!(f, "failed to install xorg related packages")
             }
         }
     }
