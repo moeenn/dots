a7247c3fc2efacfbb204cd1469c7ad5872a21dcd
moeenn <moeen.v8@gmail.com>
2025-12-12 01:12:36 +0500

    cleanup

--- programs/colors/src/main.c
+++ programs/colors/src/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-static const char* RESET = "\x1b[0m";
+static const char *RESET = "\x1b[0m";
 static const int LINES = 3;
-static const char* TEXT = "     ";
+static const char *TEXT = "     ";
 
-int main()
-{
-    puts("");
+int main() {
+  puts("");
 
-    int i, c;
-    for (i = 0; i < LINES; i++) {
-        for (c = 40; c < 48; c++) {
-            printf(" \x1b[1;%dm%s%s ", c, TEXT, RESET);
-        }
-        puts("");
+  int i, c;
+  for (i = 0; i < LINES; i++) {
+    for (c = 40; c < 48; c++) {
+      printf(" \x1b[1;%dm%s%s ", c, TEXT, RESET);
     }
-
     puts("");
-    return 0;
+  }
+
+  puts("");
+  return 0;
 }
